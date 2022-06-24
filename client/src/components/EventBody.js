4a3abf28fe8b6244520547bbc916338fb3e9161a
c2kle <c2kle@live.unc.edu>
2022-06-24 00:56:28 -0400

    new body

--- client/src/components/EventBody.js
+++ client/src/components/EventBody.js
@@ -4,13 +4,15 @@ import '../componentsStyle/eventBody.css'
 export default function EventBody() {
     return (
         <div className="eventDetails">
-            <h5>Venue: Down Time</h5>
+            <h5>Event: Mixer</h5>
+            <h5>Venue: Down Time Chapel Hill</h5>
             <h5>Date: August 26th (Friday)</h5>
             <h5>Time: 9:00pm - 11:30pm</h5>
-            <h5>Cover (Under 21): $10</h5>
-            <h5>DJ: TBA</h5>
+            <h5>Cover: $5, $10 (Under 21)</h5>
+            <h5>DJ: DJ RUSS, TBA</h5>
             <h5>Contact: silkroad@guestlist.vip</h5>
             <a className="eventRegistration" href="/silkroad-list">Guest List Registration</a>
+            <a className="eventRegistration" href="">Gallery (Post Event)</a>
         </div>
     )
 }
