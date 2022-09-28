d8bfa09375c03c15ac9feeb2bd952e9725148a52
c2kle <c2kle@live.unc.edu>
2022-09-27 23:25:21 -0400

    form css and selector

--- client/src/components/ListBody.js
+++ client/src/components/ListBody.js
@@ -11,6 +11,7 @@ export default function ListBody(props) {
     const [age, setAge] = useState("")
     const [mobile, setMobile] = useState("")
     const [promoter, setPromoter] = useState("")
+    const [selectStyle, setSelectStyle] = useState({})
 
     const [createGuest, { error, loading }] = useMutation(CREATE_GUEST)
 
@@ -20,10 +21,18 @@ export default function ListBody(props) {
     if (error) {
         return `Submission error! ${error.message}`
     }
-    
+
     const onBackHandler = () => {
         let name = "display";
-        props.setDisplayState(prevState => ({...prevState, [name]: "Event"}))
+        props.setDisplayState(prevState => ({ ...prevState, [name]: "Event" }))
+    }
+
+    const onChangeHandler = () => {
+        setSelectStyle({ color: "whitesmoke" })
+    }
+
+    const onStyleHandler = () => {
+        setSelectStyle({ color: (118,118,118) })
     }
 
 
@@ -31,27 +40,28 @@ export default function ListBody(props) {
         <div className="createGuest">
             <form>
                 <h3>SILKROAD</h3>
-                <input className="listText" type='text' placeholder='First Name'
+                <input className="listText" type='text' placeholder='Name'
                     onChange={(event) => { setFirstName(event.target.value) }}></input>
-                <input className="listText" type='text' placeholder='Last Name'
-                    onChange={(event) => { setLastName(event.target.value) }}></input>
+                {/* <input className="listText" type='text' placeholder='Last Name'
+                    onChange={(event) => { setLastName(event.target.value) }}></input> */}
+                {/* <input className="listText" type='text' placeholder='@Instagram'
+                    onChange={(event) => { setInstagram(event.target.value) }}></input> */}
+                <select style={{ ...selectStyle }} defaultValue="Age Restriction"
+                    onChange={(event) => { setAge(event.target.value); onChangeHandler() }}><option value="Age Restriction" disabled hidden>Age Restriction</option><option value="18+">18+</option><option value="21+">21+</option></select>
                 <input className="listText" type='text' placeholder='@Instagram'
-                    onChange={(event) => { setInstagram(event.target.value) }}></input>
-                <input className="listText" type='text' placeholder='Age'
-                    onChange={(event) => { setAge(event.target.value) }}></input>
-                <input className="listText" type='text' placeholder='Mobile # (Optional)'
                     onChange={(event) => { setMobile(event.target.value) }}></input>
                 <input className="listText" type='text' placeholder='Promoter (If Applicable)'
                     onChange={(event) => { setPromoter(event.target.value) }}></input>
                 <div className="listButtonsContainer">
                     {/* <button className="listButton" type="button" onClick={onBackHandler}>INFO</button> */}
                     <button className="listButton" onClick={(event) => {
-                    event.preventDefault(); createGuest({
-                        variables:
-                            { first_name: firstName, last_name: lastName, instagram: instagram, age: age, mobile: mobile, promoter: promoter }
-                    })
-                }}
-                >SUBMIT</button></div>
+                        event.preventDefault(); createGuest({
+                            variables:
+                                { first_name: firstName, last_name: lastName, instagram: instagram, age: age, mobile: mobile, promoter: promoter }
+                        })
+                            ; onStyleHandler()
+                    }}
+                    >SUBMIT</button></div>
             </form>
         </div>
     )
