fca40067b9a262444ef4b3f8a42d57e391e66aa8
c2kle <c2kle@live.unc.edu>
2022-09-27 23:50:14 -0400

    no slector , smal edits

--- client/src/components/ListBody.js
+++ client/src/components/ListBody.js
@@ -11,7 +11,7 @@ export default function ListBody(props) {
     const [age, setAge] = useState("")
     const [mobile, setMobile] = useState("")
     const [promoter, setPromoter] = useState("")
-    const [selectStyle, setSelectStyle] = useState({})
+    // const [selectStyle, setSelectStyle] = useState({})
 
     const [createGuest, { error, loading }] = useMutation(CREATE_GUEST)
 
@@ -27,28 +27,28 @@ export default function ListBody(props) {
         props.setDisplayState(prevState => ({ ...prevState, [name]: "Event" }))
     }
 
-    const onChangeHandler = () => {
-        setSelectStyle({ color: "whitesmoke" })
-    }
+    // const onChangeHandler = () => {
+    //     setSelectStyle({ color: "whitesmoke" })
+    // }
 
-    const onStyleHandler = () => {
-        setSelectStyle({ color: (118,118,118) })
-    }
+    // const onStyleHandler = () => {
+    //     setSelectStyle({ color: (118,118,118) })
+    // }
 
 
     return (
         <div className="createGuest">
             <form>
                 <h3>SILKROAD</h3>
-                <input className="listText" type='text' placeholder='Name'
+                <input className="listText" type='text' placeholder='First Name'
                     onChange={(event) => { setFirstName(event.target.value) }}></input>
-                {/* <input className="listText" type='text' placeholder='Last Name'
-                    onChange={(event) => { setLastName(event.target.value) }}></input> */}
-                {/* <input className="listText" type='text' placeholder='@Instagram'
-                    onChange={(event) => { setInstagram(event.target.value) }}></input> */}
-                <select style={{ ...selectStyle }} defaultValue="Age Restriction"
-                    onChange={(event) => { setAge(event.target.value); onChangeHandler() }}><option value="Age Restriction" disabled hidden>Age Restriction</option><option value="18+">18+</option><option value="21+">21+</option></select>
+                <input className="listText" type='text' placeholder='Last Name'
+                    onChange={(event) => { setLastName(event.target.value) }}></input>
                 <input className="listText" type='text' placeholder='@Instagram'
+                    onChange={(event) => { setInstagram(event.target.value) }}></input>
+                <input className="listText" type='text' placeholder='Age'
+                    onChange={(event) => { setAge(event.target.value) }}></input>
+                <input className="listText" type='text' placeholder='Mobile # (Optional)'
                     onChange={(event) => { setMobile(event.target.value) }}></input>
                 <input className="listText" type='text' placeholder='Promoter (If Applicable)'
                     onChange={(event) => { setPromoter(event.target.value) }}></input>
@@ -59,7 +59,6 @@ export default function ListBody(props) {
                             variables:
                                 { first_name: firstName, last_name: lastName, instagram: instagram, age: age, mobile: mobile, promoter: promoter }
                         })
-                            ; onStyleHandler()
                     }}
                     >SUBMIT</button></div>
             </form>
