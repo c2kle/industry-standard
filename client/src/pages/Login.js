0d4b519260fad55aa10c1abcd3ca43d0b66de0f8
c2kle <c2kle@live.unc.edu>
2022-09-28 02:30:10 -0400

    autofcous error

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <form >
     <input type="text" name="username" autoComplete="off" placeholder="Username" autoFocus></input>
-    <input type="text" name="password" autoComplete="off" placeholder="Password" autoFocus></input>
+    <input type="text" name="password" autoComplete="off" placeholder="Password" ></input>
     <div className="adjust-box">
       <button type="submit">ENTER</button>
     </div>
