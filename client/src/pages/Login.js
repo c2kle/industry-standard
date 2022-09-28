504cbc02a6cc5cdd78af9b1f1ca3dc6879bdd3df
c2kle <c2kle@live.unc.edu>
2022-09-28 03:03:26 -0400

    dev testing qr code with login client side

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../pageStyles/login.css'
 
 export default function Login() {
+
+    const [userName,setUserName] = useState("")
+    const [password,setPassword] = useState("")
+
+const onSubmitHandler = (e) => {
+    setUserName(e.target[0].value.toUpperCase())
+    setPassword(e.target[1].value.toUpperCase())
+}
+
+//THIS IS FOR DEV TESTING PURPOSES :)
+if (userName === 'SILKROAD' || password === 'ADMIN') {
+    return <img src="qr_test.png" style={{height: 100,width:100}}></img>
+}
   return (
-    <form >
+    <form  onSubmit={(e) => {e.preventDefault();onSubmitHandler(e)} }>
     <input type="text" name="username" autoComplete="off" placeholder="Username" autoFocus></input>
     <input type="text" name="password" autoComplete="off" placeholder="Password" ></input>
     <div className="adjust-box">
