568493ed946df9f06d3d43199aff36c4a12d5a44
c2kle <c2kle@live.unc.edu>
2022-10-03 18:42:57 -0400

    new login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,15 +17,18 @@ if (username === 'SILKROAD' || password === 'ADMIN') {
 }
   return (
     <form  className="login-form" onSubmit={(e) => {e.preventDefault();onSubmitHandler(e)} }>
-    <input className="login-form__input" type="password" name="space" autoComplete="off" disabled></input>
-    <input className="login-form__input" type="text" name="username" autoComplete="off" placeholder="Username"></input>
-    <input className="login-form__input" type="password" name="password" autoComplete="off" placeholder="Password"></input>
+    <input className="login-form__input" type="password" name="safariSpace" autoComplete="off" disabled></input>
+    <div className="login-form__input-div"><label className="login-form__label" name="username-label">Username</label>
+    <input className="login-form__input" type="text" name="username" autoComplete="off"></input></div>
+    <div className="login-form__input-div"><label className="login-form__label" name="username-label">Password</label>
+    <input className="login-form__input" type="password" name="password" autoComplete="off"></input></div>
+    
     <div className="login-form__adjust-box">
       <label className="login-form__adjust-box__checkbox-label"><input className="login-form__adjust-box__checkbox-label__checkbox" type="checkbox"></input><span className="login-form__adjust-box__checkbox-label__span">Remember me</span></label>
-      <button className="login-form__adjust-box__button"type="submit">Forgot password?</button>
+      <button className="login-form__adjust-box__button"type="submit">SIGN IN</button>
     </div>
-    <button className="login-form__enter-button" type="submit">ENTER</button>
-    <div className="login-form__space-div"></div>
+    <button className="login-form__enter-button" type="submit"></button>
+    <div className="login-form__space-div"><span className="login-form__space-div__span">Register</span><span> | </span><span className="login-form__space-div__span">Forgot password?</span></div>
   </form>
   )
 }
