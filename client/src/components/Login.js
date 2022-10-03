f4c7f7f8b4938f5f34dc3ed09728c8ffbf858b73
c2kle <c2kle@live.unc.edu>
2022-10-03 02:35:39 -0400

    scrol on focus

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,8 +7,21 @@ export default function Login() {
     const [password,setPassword] = useState("")
 
 const onSubmitHandler = (e) => {
-    setUsername(e.target.value.toUpperCase())
-    setPassword(e.target.value.toUpperCase())
+    setUsername(e.target[0].value.toUpperCase())
+    setPassword(e.target[1].value.toUpperCase())
+}
+
+const onClickFocus1 = (e) => {
+  e.target.scrollIntoView()
+}
+
+const onClickFocus2 = (e) => {
+  e.target.scrollTop()
+}
+
+const onClickFocus3 = (e) => {
+  window.scrollTo(0,0)
+  e.target.scrollTop()
 }
 
 //THIS IS FOR DEV TESTING PURPOSES :)
@@ -17,13 +30,14 @@ if (username === 'SILKROAD' || password === 'ADMIN') {
 }
   return (
     <form  className="login-form" onSubmit={(e) => {e.preventDefault();onSubmitHandler(e)} }>
-    <input className="login-form__input" type="text" name="username" autoComplete="off" placeholder="Username"></input>
-    <input className="login-form__input" type="password" name="password" autoComplete="off" placeholder="Password"></input>
+    <input className="login-form__input" type="text" name="username" autoComplete="off" placeholder="Username" onClick={onClickFocus1}></input>
+    <input className="login-form__input" type="password" name="password" autoComplete="off" placeholder="Password" onClick={onClickFocus2}></input>
+    <input className="login-form__input" type="text" name="register" autoComplete="off" placeholder="register" onClick={onClickFocus3}></input>
     <div className="login-form__adjust-box">
       <label className="login-form__adjust-box__checkbox-label"><input className="login-form__adjust-box__checkbox-label__checkbox" type="checkbox"></input><span className="login-form__adjust-box__checkbox-label__span">Remember me</span></label>
       <button className="login-form__adjust-box__button"type="submit">Forgot password?</button>
     </div>
-    <button className="login-form__enter-button">ENTER</button>
+    <button className="login-form__enter-button" type="submit">ENTER</button>
   </form>
   )
 }
