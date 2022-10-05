6060ed87e5f394bfb1f0fd02c192887368439896
c2kle <c2kle@live.unc.edu>
2022-10-05 19:13:19 -0400

    animate header home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,12 +4,10 @@ import "../componentsStyle/home.css"
 export default function Home() {
   return (
     <div className="home-div">
-      <div className="home-div__div-1"> 
-        <h1 className="home-h1">EXCLUSIVE<br></br>DIGITAL<br></br>INVITATIONS<br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+</h1>
-        <h1 className="home-h1"><br></br>GUEST LIST<br></br><br></br>RSVP</h1>
-      </div>
-      <div>
-      </div>
+      <h1 className="home-h1" name="rotate"></h1>
+      <h1 className="home-h1">INVITATIONS</h1>
+      <h1 className="home-h1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GUEST LIST</h1>
+      <h1 className="home-h1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RSVP</h1>
     </div>
   )
 }
