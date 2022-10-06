f1cdf0fcbb30dcc20d0d21af3d4aae44d5625482
c2kle <c2kle@live.unc.edu>
2022-10-05 21:34:52 -0400

    trial

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ import "../componentsStyle/home.css"
 export default function Home() {
   return (
     <div className="home-div">
-      <h1 className="home-h1" name="rotate"></h1>
+      <h1 className="home-h1-animate"></h1>
       <h1 className="home-h1">INVITATIONS</h1>
       <h1 className="home-h1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GUEST LIST</h1>
       <h1 className="home-h1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RSVP</h1>
