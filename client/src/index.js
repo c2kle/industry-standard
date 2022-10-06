e4df66975ab8fe5102adbdbfdfb3c1c86830d0e5
c2kle <c2kle@live.unc.edu>
2022-10-06 00:20:38 -0400

    font face

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "./fonts/Be_Vietnam_Pro/BeVietnamPro-ExtraLight.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
